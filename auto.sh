50891f64c5e91ba9fe23b5fc927380de71992f27
Socapex <philippe.groarke@gmail.com>
2015-10-08 04:47:52 -0400

    Test

--- auto.sh
+++ auto.sh
@@ -4,30 +4,37 @@ function commit_submodules
 {
 
         for dir in $(find modules/ -type d -maxdepth 1); do
-                cd -v $dir;
+                cd $dir;
+                echo -e "\n###\nProcessing $(pwd) \n###\n";
                 git add .;
                 git commit -m "$1";
                 git push;
-                cd -v -;
+                cd -;
         done
+}
+
+function clean
+{
+        git ls-files > keep-these.txt;
+        git filter-branch --force --index-filter \
+                  "git rm  --ignore-unmatch --cached -qr . ; \
+                    cat $PWD/keep-these.txt | xargs git reset -q \$GIT_COMMIT --" \
+                      --prune-empty --tag-name-filter cat -- --all;
+        rm -rf .git/refs/original/;
+        git reflog expire --expire=now --all;
+        git gc --prune=now;
+        git gc --aggressive --prune=now;
+        rm keep-these.txt;
 }
 
 function clean_history
 {
         for dir in $(find modules/ -type d -maxdepth 1); do
-                cd -v $dir;
-                git ls-files > keep-these.txt;
-                git filter-branch --force --index-filter \
-                          "git rm  --ignore-unmatch --cached -qr . ; \
-                            cat $PWD/keep-these.txt | xargs git reset -q \$GIT_COMMIT --" \
-                              --prune-empty --tag-name-filter cat -- --all;
-                rm -rf .git/refs/original/;
-                git reflog expire --expire=now --all;
-                git gc --prune=now;
-                git gc --aggressive --prune=now;
-                rm keep-these.txt;
-                cd -v -;
+                cd $dir;
+                clean;
+                cd -;
         done
+        clean;
 }
 
 if [ "$1" == "clean_history" ]; then
